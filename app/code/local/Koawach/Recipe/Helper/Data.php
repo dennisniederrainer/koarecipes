3503ea2a4b33eb93639680b2d0e1e33faaf8fd46
dennisniederrainer <dennis.niederrainer@gmail.com>
2016-08-07 12:26:18 +0200

    diesdas

---
+++ app/code/local/Koawach/Recipe/Helper/Data.php
@@ -0,0 +1,3 @@
+<?php
+
+class Koawach_Recipes_Helper_Data extends Mage_Core_Helper_Abstract {}
---
+++ app/code/local/Koawach/Recipe/sql/recipes_setup/mysql4-install-0.1.0.php
@@ -0,0 +1,32 @@
+<?php
+
+die('springt.');
+$installer = $this;
+
+$installer->startSetup();
+
+/**
+ * create recipe table
+ */
+$installer->run("
+
+DROP TABLE IF EXISTS {$this->getTable('recipes')};
+
+CREATE TABLE {$this->getTable('recipes')} (
+  `recipe_id` int(11) unsigned NOT NULL auto_increment,
+  `name` varchar(255) NOT NULL default '',
+  `type_id` int(11) NOT NULL default '0',
+
+  `description` varchar(255) NOT NULL default '',
+  `ingredients` varchar(255) NOT NULL default '',
+  `duration` int(8) NULL,
+  `number_of_person` int(8) NULL,
+  `author` varchar(255) NOT NULL default '',
+  `video_url` varchar(255) NOT NULL default '',
+
+  PRIMARY KEY (`recipe_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
+
+");
+
+$installer->endSetup();
